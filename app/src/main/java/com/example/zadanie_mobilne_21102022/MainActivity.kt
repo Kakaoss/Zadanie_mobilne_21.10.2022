eec400ecc7cbd275d56e3d5ce763346c582ccb2c
snaketrap <b.sarnecki11@gmail.com>
2022-10-21 14:57:37 +0200

    skonczone zadanie, dodanie funkcjonalnosci przyciskow : dodawanie tekstu oraz przywracanie do pierwotnego stanu :)

--- app/src/main/java/com/example/zadanie_mobilne_21102022/MainActivity.kt
+++ app/src/main/java/com/example/zadanie_mobilne_21102022/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.zadanie_mobilne_21102022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dodaj = findViewById<Button>(R.id.btnDodaj)
+        val przywroc = findViewById<Button>(R.id.btnPrzywroc)
+        val text = findViewById<TextView>(R.id.imie_nazwisko)
+        val dodawanytext = findViewById<EditText>(R.id.wprowadzanie)
+
+        dodaj.setOnClickListener {
+            text.text = text.text.toString() + dodawanytext.text.toString()
+        }
+        przywroc.setOnClickListener {
+            text.text = "Jakub Wyczanowski"
+        }
     }
 }
